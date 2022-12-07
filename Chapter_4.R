9566e80c5e45c13e02b038e0e4042a9ab15b68c1
m-jurzyk <jurzyk.maciej@gmail.com>
2022-12-07 14:35:51 +0100

    Chapter_5

---
+++ Chapter_4.R
@@ -0,0 +1,26 @@
+## CHAPTER4- Workflow basics----
+
+### 4.4 Workflow basics - Excercises----
+
+#Why does this code not work?
+
+my_variable <- 10
+
+my_variable
+my_variable
+
+# letter 'i' was broken 
+
+library(tidyverse)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
+
+## Fixed 
+
+
+## alt shift + k - show shourtcuts 
+

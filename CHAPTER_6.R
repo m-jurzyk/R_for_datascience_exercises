570906534ff872828e89b913fd9aee1a4f818ddb
m-jurzyk <jurzyk.maciej@gmail.com>
2022-12-09 12:05:12 +0100

    Chapter 6 - read 8.12.2022

---
+++ CHAPTER_6.R
@@ -0,0 +1,15 @@
+# CHAPTER- 6 Scripts 
+
+library(dplyr)
+library(nycflights13)
+
+
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
+
+
+## END 

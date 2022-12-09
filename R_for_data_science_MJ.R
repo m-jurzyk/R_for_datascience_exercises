383461db1cd30da4320a8b8dd2526a1df404ac92
m-jurzyk <jurzyk.maciej@gmail.com>
2022-12-09 12:05:41 +0100

    Chapter 7,8 - Read and made 9.12.2022 -Some problems with more complicated excercises

--- R_for_data_science_MJ.R
+++ R_for_data_science_MJ.R
@@ -1,6 +1,6 @@
 # R for data science ----
 
-
+#EXPLORE ----
 ## Loading library tidiverse to get access to ggplot
 
 library(tidyverse)
@@ -636,3 +636,201 @@ ggplot(data = delays, mapping = aes(x = delay)) +
 
 
 # CHAPTER 6-Workflow: scripts ----
+
+# CHAPTER- 6 Scripts 
+
+library(dplyr)
+library(nycflights13)
+
+
+not_cancelled <- flights %>% 
+  filter(!is.na(dep_delay), !is.na(arr_delay))
+
+not_cancelled %>% 
+  group_by(year, month, day) %>% 
+  summarise(mean = mean(dep_delay))
+
+
+## Chapter about workflow and sharing scripts, valuable information
+
+
+## END 
+
+# CHAPTER 7-Exploratory data analysis ----
+
+## Histogram 
+
+library(tidyverse)
+
+ggplot(data = diamonds) +
+  geom_histogram(mapping = aes(x = carat), binwidth = 0.5)
+
+
+smaller <- diamonds %>% 
+  filter(carat < 3)
+
+ggplot(data = smaller, mapping = aes(x = carat)) +
+  geom_histogram(binwidth = 0.1)
+
+## It's better to use geom_freqpoly then histogram to show more lines 
+
+ggplot(data = smaller, mapping = aes(x = carat, colour = cut)) +
+  geom_freqpoly(binwidth = 0.1)
+
+ggplot(data = diamonds, mapping = aes(x = carat)) +
+  geom_histogram(binwidth = 0.01)
+
+
+
+
+# Detection of unusual values 
+
+unusual <- diamonds %>% 
+  filter(y < 3 | y > 20) %>% 
+  select(price, x, y, z) %>%
+  arrange(y)
+
+unusual
+
+###7.3.4 Exercises ----
+
+#Explore the distribution of each of the x, y, and z variables in diamonds.
+#What do you learn? Think about a diamond and how you might decide which dimension
+#is the length, width, and depth.
+
+?diamonds
+
+#x- length in mm (0–10.74)
+
+diamonds %>%  ggplot(mapping = aes(x=x))+
+  geom_histogram(binwidth = 0.01)
+
+
+#y = width in mm (0–58.9)
+
+diamonds %>%  ggplot(mapping = aes(x=y))+
+  geom_histogram(binwidth = 0.01)
+
+
+#z = depth in mm (0–31.8)
+
+diamonds %>%  ggplot(mapping = aes(x=z))+
+  geom_histogram(binwidth = 0.01)
+
+
+# It's all about shape of diamond 
+
+#Explore the distribution of price. Do you discover anything unusual or surprising?
+#(Hint: Carefully think about the binwidth and make sure you try a wide range of values.)
+
+
+diamonds %>%  ggplot(mapping = aes(x=price))+
+  geom_histogram(binwidth = 0.1)
+
+diamonds %>%  ggplot(mapping = aes(x=price))+
+  geom_histogram(binwidth = 1)
+
+
+## How many diamonds are 0.99 carat? How many are 1 carat?
+#What do you think is the cause of the difference?
+
+p1carat <- diamonds %>% 
+  filter(carat==1) %>% 
+  select(price,carat) %>%
+  arrange(price) %>% 
+  count()
+
+p1carat
+
+p2carat <- diamonds %>% 
+  filter(carat==0.99) %>% 
+  select(price,carat) %>%
+  arrange(price) %>% 
+  count()
+
+p2carat
+
+# 1558 diamonds are 1 carat and olny 23 are 0.99 carat 
+
+
+## Missing values 
+
+# drop row with strange values 
+
+
+diamonds2 <- diamonds %>% 
+filter(between(y, 3, 20))
+
+diamonds2
+
+# But better is to put NA insted of missing values 
+
+diamonds2 <- diamonds %>% 
+  mutate(y = ifelse(y < 3 | y > 20, NA, y))
+
+diamonds2
+
+
+ggplot(data = diamonds2, mapping = aes(x = x, y = y)) + 
+  geom_point()
+
+## WARNING Komunikat ostrzegawczy:Removed 9 rows containing missing values (`geom_point()`)
+
+ggplot(data = diamonds2, mapping = aes(x = x, y = y)) + 
+  geom_point(na.rm = TRUE)
+
+## Without warning 
+
+## 
+
+nycflights13::flights %>% 
+  mutate(
+    cancelled = is.na(dep_time),
+    sched_hour = sched_dep_time %/% 100,
+    sched_min = sched_dep_time %% 100,
+    sched_dep_time = sched_hour + sched_min / 60
+  ) %>% 
+  ggplot(mapping = aes(sched_dep_time)) + 
+  geom_freqpoly(mapping = aes(colour = cancelled), binwidth = 1/4)
+
+
+ggplot(data = diamonds, mapping = aes(x = price)) + 
+  geom_freqpoly(mapping = aes(colour = cut), binwidth = 500)
+
+ggplot(diamonds) + 
+  geom_bar(mapping = aes(x = cut))
+
+ggplot(data = diamonds, mapping = aes(x = price, y = ..density..)) + 
+  geom_freqpoly(mapping = aes(colour = cut), binwidth = 500)
+
+## BOXPLOT 
+ggplot(data = diamonds, mapping = aes(x = cut, y = price)) +
+  geom_boxplot()
+
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) +
+  geom_boxplot()
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = reorder(class, hwy, FUN = median), y = hwy))
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = reorder(class, hwy, FUN = median), y = hwy)) +
+  coord_flip()
+
+ggplot(data = diamonds) + 
+  geom_point(mapping = aes(x = carat, y = price), alpha = 1 / 100)
+ggplot(data = smaller, mapping = aes(x = carat, y = price)) + 
+  geom_boxplot(mapping = aes(group = cut_width(carat, 0.1)))
+
+
+# CHAPTER 8-Workflow: projects ----
+
+## Press Cmd/Ctrl + Shift + F10 to restart RStudio.
+#Press Cmd/Ctrl + Shift + S to rerun the current script.
+
+
+# WRANGLE  ----
+
+## CHAPTER 9- Introduction ----
+
+

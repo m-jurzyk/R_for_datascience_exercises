525d47621c70c431ecf0805fb10dbe419496a40f
m-jurzyk <jurzyk.maciej@gmail.com>
2023-02-04 22:23:57 +0100

    Chapter 14 - regular expression exercises

--- R_for_data_science_MJ.R
+++ R_for_data_science_MJ.R
@@ -1238,6 +1238,21 @@ str_view(x,"\\\\")
 #the third will escape the next character.
 #So in the regular expression, this will escape some escaped character.
 
+str_length("s123sdaweqwe") # Number of characters in string 
+
+c <- "s123sdaweqwe"
+
+str_length(c)
+
+str_to_upper(c)
+
+str_to_title(c)
+
+d <- "make a sentence for ME"
+
+str_to_sentence(d)
+
+str_trim("      TRIM    ")
 ## Found on solution book*** 
 
 #Exercise 14.3.1
@@ -1254,7 +1269,7 @@ x <- c("apple", "banana", "pear")
 
 str_view(x, "a$") # The end of the string
 
-str_view(x, "^a") # Start of the string 
+str_view(x, "^a") # Start of the string
 
 #To remember which is which, try this mnemonic which I learned from Evan Misshula: 
   #if you begin with power (^), you end up with money ($).
@@ -1271,6 +1286,8 @@ str_view(x, "^apple$")
 #create regular expressions that find all words that:
 
 x <- c(words)
+
+head(words)
 
 # Ends with X
 
@@ -1296,6 +1313,8 @@ str_view(c("grey","gray"), "gr(e|a)y")
 
 #Start with a vowel.
 
+x <- words
+
 str_view(x,"^[a,e,i,o,u,y]", match=TRUE)
 
 str_subset(stringr::words, "^[aeiou]")
@@ -1311,3 +1330,152 @@ str_subset(stringr::words, "[^e]ed$")
 #Words ending in ing or ise:
 
 str_subset(stringr::words, "[i(ng|se)]$")
+
+str_subset(x,"[aeiou]", negate=TRUE)
+
+#Words that end with "ed"
+
+str_view(x,"ed$", match=TRUE)
+
+#Words that end with "ed, but not ending in "eed".
+
+str_view(x,"[^e]ed$", match=TRUE)
+
+#Words ending in "ING" or "ISE"
+
+str_view(x,"i(ng|se)", match=TRUE)
+
+
+#Is Q always followed by a "u"
+
+str_view(x,"q[^u]", match=TRUE)
+
+# Create a regular expression that will match telephone numbers as commonly
+#written in your country.
+
+x <- c("123-456-789", "(123)456-789", "(123) 456-780", "123-125-251")
+
+str_view(x, "(\\(?\\d{3})")
+
+
+## Regular Expressions in R - exercises 
+
+
+letters = c("abc", "abdd", "abcd", "xyz")
+str_match(letters, "abc")
+
+numbers = c("42", "421", "411!", "24")
+
+str_match(numbers, "42")
+
+
+str_match(phone_numbers, pattern)
+
+
+
+##14.x.x----Excercises with regexr.com / regegex one 
+
+
+##14.3.4 Repetition ----
+
+# How many times pattern matches 
+
+#? 0 or 1 
+#+ 1 or more
+#* 0 or more 
+
+
+x <- "1888 is the longest year in Roman numerals: MDCCCLXXXVIII"
+
+str_view(x, "C[LX]+")
+
+## 14.3.4.1 Exercises ---- 
+
+#1_
+
+# ?={0,1}
+# +={1,}
+# *={0,}
+
+#2_
+
+#^.*$ - every string 
+
+#"\\{.+\\}" - will match every string with sing in {}
+
+#\d{4}-\d{2}-\d{2} - 4 digits, 2 digits, 2 digits,
+
+#"\\\\{4}" - \\\\\\\\ - four backslashes 
+
+
+
+#Create regular expressions to find all words that:
+
+#Start with three consonants.
+
+str_view(words, "^[^aeiou]{3}", match= TRUE)
+
+#Have three or more vowels in a row.
+
+str_view(words, "[^aeiou]{3,}", match= TRUE)
+
+#Have two or more vowel-consonant pairs in a row.
+
+str_view(words, "([aeiou][^aeiou]){2,}", match= TRUE)
+
+##14.3.5 Grouping and backreferences ----
+
+str_view(fruit, "(..)\\1", match = TRUE)
+
+#(.)\1\1 _ "(.)(.)\\2\\1"_ (..)\1: _ "(.).\\1.\\1": _ "(.)(.)(.).*\\3\\2\\1"
+
+#1.(.)\1\1: The same character appearing three times in a row. E.g. "aaa"
+#2."(.)(.)\\2\\1": A pair of characters followed by the
+#same pair of characters in reversed order. E.g. "abba".
+#3. (..)\1: Any two characters repeated. E.g. "a1a1".
+#4. (.).\\1.\\1": A character followed by any character
+#the original character, any other character, the original character again.
+#E.g. "abaca", "b8b.b".
+#5 "(.)(.)(.).*\\3\\2\\1" Three characters followed by zero or more 
+#characters of any kind followed by the same three characters but
+#in reverse order. E.g. "abcsgasgddsadgsdgcba" or "abccba" or "abc1cba".
+
+## Solution: https://jrnold.github.io/r4ds-exercise-solutions/strings.html
+
+#Construct regular expressions to match words that:
+
+#Start and end with the same character.
+
+str_subset(words, "^(.)((.*\\1$)|\\1?$)")
+
+
+## 14.4.1 Exercises ---- 
+
+#Find all words that start or end with x.
+
+words[str_detect(words,"^x|x$")]
+
+#Find all words that start with a vowel and end with a consonant.
+
+words[str_detect(words,"^[aeiou].*[^aeiou]$")]
+
+#Are there any words that contain at least one of each different vowel?
+
+words[str_detect(words,"[a].*[e].*[i].*[o].*[u]")]
+
+# O words 
+
+#What word has the higher number of vowels? What word has the highest proportion of vowels?
+#(Hint: what is the denominator?)
+
+vowels <-str_count(words,"[aeiou]")
+
+words[which(vowels == max(vowels))]
+
+prop_vowels <-str_count(words,"[aeiou]") / str_length(words)
+
+words[which(prop_vowels == max(prop_vowels))]
+
+# but how to delete "a"?
+
+

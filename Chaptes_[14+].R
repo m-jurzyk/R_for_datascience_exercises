5fe74e8d94ea06b5c977e5a051bf264fc74e4ae5
m-jurzyk <jurzyk.maciej@gmail.com>
2023-02-07 15:07:50 +0100

    ## Tools subchapter finished

--- Chaptes_[14+].R
+++ Chaptes_[14+].R
@@ -1,4 +1,3 @@
-
 #CHAPTER 14- STRINGR ----
 
 
@@ -139,7 +138,7 @@ str_view(x, "apple")
 str_view(x, "^apple$")
 
 
-##14.3.2Exercises ---- 
+###14.3.2Exercises ---- 
 
 #Given the corpus of common words in stringr::words, 
 #create regular expressions that find all words that:
@@ -166,7 +165,7 @@ str_view(x, "........", match = TRUE)
 
 str_view(c("grey","gray"), "gr(e|a)y")
 
-##14.3.3 Exercises----
+###14.3.3 Exercises----
 
 #Create regular expressions to find all words that:
 
@@ -248,7 +247,7 @@ x <- "1888 is the longest year in Roman numerals: MDCCCLXXXVIII"
 
 str_view(x, "C[LX]+")
 
-## 14.3.4.1 Exercises ---- 
+### 14.3.4.1 Exercises ---- 
 
 #1_
 
@@ -308,7 +307,7 @@ str_view(fruit, "(..)\\1", match = TRUE)
 str_subset(words, "^(.)((.*\\1$)|\\1?$)")
 
 
-## 14.4.1 Exercises ---- 
+### 14.4.1 Exercises ---- 
 
 #Find all words that start or end with x.
 
@@ -336,3 +335,194 @@ prop_vowels <-str_count(words,"[aeiou]") / str_length(words)
 words[which(prop_vowels == max(prop_vowels))]
 
 # but how to delete "a"?
+
+x <- c("apple", "banana", "pear")
+str_count(x, "a")
+
+
+##14.4.2 Extract matches ---- 
+
+stringr::sentences# Harvard sentences 
+
+colors <- c("red", "oragne", "yellow", "green", "blue", "purple")
+
+color_match <- str_c(colors, collapse = "|")
+
+color_match
+
+has_color <- str_subset(sentences, color_match)
+
+matches <- str_extract(has_color, color_match)
+
+matches
+
+more <- sentences[str_count(sentences, color_match)>1]
+
+str_view_all(more,color_match)
+
+str_extract_all(more,color_match, simplify = TRUE)
+
+x <- c("a", "a b", "a b c")
+
+str_extract_all(x, "[a-z]", simplify = TRUE)
+
+
+###14.4.2.1 Exercises ---- 
+
+#From the Harvard sentences data, extract:
+
+#The first word from each sentence.
+
+str_extract(sentences, "^\\w+")
+
+#All words ending in ing.
+
+str_extract(sentences, "^\\w+ing")
+
+enging_ing <- ("^\\w+ing")
+
+sentences %>% 
+  str_subset(enging_ing) %>% 
+  head()
+
+str_match(sentences, enging_ing)
+
+
+#All plurals
+
+str_extract(sentences, "^\\w+s")  # not exactly 
+
+##14.4.3 Grouped matches ---- 
+
+noun <- "(a|the) ([^ ]+)"
+
+str_extract(sentences, noun)
+
+has_noun <- sentences %>%
+  str_subset(noun) %>%
+  head(10)
+
+
+has_noun %>% 
+  str_match(noun)
+
+
+tibble(sentence = sentences) %>% 
+  tidyr::extract(
+    sentence, c("article", "noun"), "(a|the) ([^ ]+)", 
+    remove = FALSE
+  )
+
+
+###14.4.3.1 Exercises ----
+
+#Find all words that come after a “number” like “one”, “two”, “three” etc. 
+#Pull out both the number and the word.
+
+numbers <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+numbers_match <- str_c(numbers, collapse = "|")
+
+numbers_match
+
+numword <- "\\b(one|two|three|four|five|six|seven|eight|nine) +(\\w+)"
+
+has_number <- str_subset(sentences, numword)
+
+has_number %>% head()
+
+
+#Find all contractions. Separate out the pieces before and after the apostrophe.
+
+
+contraction <- "([\\w]+)'([\\w+]+)"
+
+sentences[str_detect(sentences, contraction)] %>%
+  str_extract(contraction) %>% 
+  str_split("'")
+
+##14.4.4 Replacing matches----
+
+x <- c("apple", "pear", "banana")
+str_replace(x, "[aeiou]", "-")
+
+str_replace_all(x, "[aeiou]", "-")
+
+x <- c("1 house", "2 cars", "3 people")
+str_replace_all(x, c("1" = "one", "2" = "two", "3" = "three"))
+
+sentences %>% 
+  str_replace("([^ ]+) ([^ ]+) ([^ ]+)", "\\1 \\3 \\2") %>% 
+  head(5)
+
+###14.4.4.1 Exercises ----
+
+#Replace all forward slashes in a string with backslashes.
+
+str_replace_all("past/present/future", "/", "\\\\")
+
+#Implement a simple version of str_to_lower() using replace_all().
+
+str_replace_all("Maciej, Jurzyk", "[A-Z]", "[a-z]") # not working 
+
+name <- c("Maciej", "Jurzyk")
+
+replacements <- c("A" = "a", "B" = "b", "C" = "c", "D" = "d", "E" = "e",
+                  "F" = "f", "G" = "g", "H" = "h", "I" = "i", "J" = "j", 
+                  "K" = "k", "L" = "l", "M" = "m", "N" = "n", "O" = "o", 
+                  "P" = "p", "Q" = "q", "R" = "r", "S" = "s", "T" = "t", 
+                  "U" = "u", "V" = "v", "W" = "w", "X" = "x", "Y" = "y", 
+                  "Z" = "z")
+
+lower_words <- str_replace_all(name, pattern = replacements)
+head(lower_words)
+
+#Switch the first and last letters in words. Which of those strings are still words?
+
+swapped <- str_replace_all(words, "^([A-Za-z])(.*)([A-Za-z])$", "\\3\\2\\1")
+
+intersect(swapped, words)
+
+##14.4.5 Splitting----
+
+sentences %>%
+  head(5) %>% 
+  str_split(" ")
+
+"a|b|c|d" %>% 
+  str_split("\\|") %>% 
+  .[[1]]
+
+sentences %>%
+  head(5) %>% 
+  str_split(" ", simplify = TRUE)
+
+fields <- c("Name: Hadley", "Country: NZ", "Age: 35")
+fields %>% str_split(": ", n = 2, simplify = TRUE)
+
+x <- "This is a sentence.  This is another sentence."
+str_view_all(x, boundary("word"))
+
+##14.4.5.1 Exercises ----
+
+#Split up a string like "apples, pears, and bananas" into individual components.
+
+x <- c("apples", "pears", "bananas")
+
+str_split(x, ", +(and +)?")[[1]]
+
+
+#Why is it better to split up by boundary("word") than " "?
+
+#What does splitting with an empty string ("") do?
+#Experiment, and then read the documentation.
+
+##14.4.6 Find matches ----
+
+#str_locate() and str_locate_all() give you the starting and ending 
+#positions of each match. These are particularly useful when none of the other 
+#functions does exactly what you want. You can use str_locate() to find the 
+#matching pattern, str_sub() to extract and/or modify them.
+
+
+##14.5 Other types of pattern ----
